1737c1939fa3aa8c64c9197f9071a95bf89882d7
Sakshi Dhondkar <122208559+sakshidhondkar@users.noreply.github.com>
2023-08-07 22:41:53 +0530

    Create Day-7.java

---
+++ Day-7.java
@@ -0,0 +1,39 @@
+/*
+  Rotate the array left N times. Accept N value from the user.
+*/
+
+import java.util.Scanner;
+
+public class Day_6 {
+
+	
+	 
+	    public static void main(String[] args) {  
+	         Scanner sc= new Scanner(System.in);
+	        int [] arr = new int [] {1, 2, 3, 4, 5};  
+	        System.out.println("Enter position:");
+	        int n =sc.nextInt();  
+	        
+	        System.out.println("Original array: ");  
+	        for (int i = 0; i < arr.length; i++) {  
+	            System.out.print(arr[i] + " ");  
+	        }  
+	         
+	        for(int i = 0; i < n; i++){  
+	            int j, first;  
+	            //Stores the first element of the array  
+	            first = arr[0];  
+	            for(j = 0; j < arr.length-1; j++)
+	            { 	                
+	                arr[j] = arr[j+1];  
+	            }	           
+	            arr[j] = first; 
+	        }  
+	        System.out.println();  
+	        
+	        System.out.println("Array after left rotation: ");  
+	        for(int i = 0; i< arr.length; i++){  
+	            System.out.print(arr[i] + " ");  
+	        }  
+	    }  
+	}  
